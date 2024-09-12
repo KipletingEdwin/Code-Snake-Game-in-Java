93a4bebe77b9f4130b96e6d809f91a9cc206b629
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-12 22:53:50 +0300

    create a constructor

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,2 +1,13 @@
-public class SnakeGame {
+import javax.swing.*;
+
+public class SnakeGame extends JPanel {
+
+    int boardWidth;
+    int boardHeight;
+
+    SnakeGame(int boardWidth, int boardHeight) {
+
+    }
+
+
 }
