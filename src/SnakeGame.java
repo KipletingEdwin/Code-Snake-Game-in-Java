3f36fc263a6a5706a30bc2939d3d55cbfb9659d4
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-09-14 09:30:14 +0300

    adda private class called Tile

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,8 +3,20 @@ import java.awt.*;
 
 public class SnakeGame extends JPanel {
 
+    private  class Tile{
+        int x;
+        int y;
+
+        Tile(int x, int y){
+            this.x = x;
+            this.y = y;
+
+        }
+    }
+
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
 
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
